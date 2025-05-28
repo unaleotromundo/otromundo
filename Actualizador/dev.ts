6baf90f5b6c2e09b13b4100b5f278e67b0b6c362
aleotromundo <contralamaquina@gmail.com>
2025-05-28 17:27:09 -0300

    fotos

--- Actualizador/dev.ts
+++
@@ -1,1 +0,0 @@
-import { config } from 'dotenv';config();import '@/ai/flows/generate-plant-description.ts';import '@/ai/flows/generate-plant-name.ts';import '@/ai/flows/generate-pdf-background.ts';
